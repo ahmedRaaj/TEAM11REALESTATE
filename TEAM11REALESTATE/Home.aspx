5d367fdb0550c5d86a49ad59f1eab3e017f1c906
DESKTOP-BT5DJ0U\Zwe Htat Naing <zwehtatnaing.liveid@gmail.com>
2016-11-07 16:49:25 +0800

    Changes

--- TEAM11REALESTATE/Home.aspx
+++ TEAM11REALESTATE/Home.aspx
@@ -1,3 +1,3 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="TEAM11REALESTATE.Home" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+﻿<%@ Page Title="" Language="C#" MasterPageFile="MainPage.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="TEAM11REALESTATE.Home" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainPage" runat="server">
 </asp:Content>
---
+++ TEAM11REALESTATE/MainPage.Master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+
+namespace TEAM11REALESTATE
+{
+    public partial class MainPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
+        {
+            Context.GetOwinContext().Authentication.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+        }
+    }
+}
