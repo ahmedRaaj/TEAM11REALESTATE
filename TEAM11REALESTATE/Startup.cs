0a38b035216f998e643d2355247a5884c9f7b6b8
AHMEDRAAJ\ahmed <ahmedraaj@gmail.com>
2016-11-07 13:39:51 +0800

    Add project files.

---
+++ TEAM11REALESTATE/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TEAM11REALESTATE.Startup))]
+namespace TEAM11REALESTATE
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
