4ce412e30c305cbb5ae0d77ee880b53437429fe3
KaungMyatLin <e0046786@u.nus.edu>
2016-11-08 16:43:28 +0800

    AgencyEDediting

--- TEAM11REALESTATE/Agencies/AgencyEditDelete.aspx.cs
+++ TEAM11REALESTATE/Agencies/AgencyEditDelete.aspx.cs
@@ -10,9 +10,9 @@ namespace TEAM11REALESTATE.Agent_s_Agencies
 {
     public partial class AgencyEditDelete : System.Web.UI.Page
     {
+        RealEstateModel context = new Models.RealEstateModel();
         protected void Page_Load(object sender, EventArgs e)
         {
-            RealEstateModel context = new Models.RealEstateModel();
             Agency agnc = context.Agencies.Where(x => x.AgencyName.Equals(tbx_AgencyName.Text)).First();
             tbx_AgencyName.Text = agnc.AgencyName;
             tbx_UnitNumber.Text = agnc.Address.UnitNumber;
@@ -22,16 +22,23 @@ namespace TEAM11REALESTATE.Agent_s_Agencies
             tbx_Mobile.Text = agnc.Address.Mobile;
             tbx_OfficePh.Text = agnc.Address.Phone;
             tbx_Website.Text = agnc.Address.Website;
+
         }
 
         protected void btn_Update_Click(object sender, EventArgs e)
         {
+            Agency agnc = context.Agencies.Where(x => x.AgencyName.Equals(tbx_AgencyName.Text)).First();
+            agnc.Address.UnitNumber = tbx_UnitNumber.Text;
+            agnc.Address.BlockNumber = tbx_BlockNumber.Text;
+            agnc.Address.StreetName = tbx_StreetName.Text;
+            agnc.Address.PostalCode = tbx_PostalCode.Text;
+            agnc.Address.Email = tbx_Email.Text;
+            agnc.Address.Mobile = tbx_Mobile.Text;
+            agnc.Address.Phone = tbx_OfficePh.Text;
+            agnc.Address.Website = tbx_Website.Text;
 
-        }
-
-        protected void btn_Clear_Click(object sender, EventArgs e)
-        {
-
+            context.Agencies.Add(agnc);
+            context.SaveChanges();
         }
     }
 }
