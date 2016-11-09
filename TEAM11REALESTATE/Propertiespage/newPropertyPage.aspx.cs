5bd05c0fe7d815887d3d6242bb2dcb014fc5180e
DESKTOP-BHB5F60\Zoe Wee <zozowee1@gmail.com>
2016-11-09 14:42:28 +0800

    Property Modification

---
+++ TEAM11REALESTATE/Propertiespage/newPropertyPage.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TEAM11REALESTATE.Propertiespage
+{
+    public partial class newPropertyPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
